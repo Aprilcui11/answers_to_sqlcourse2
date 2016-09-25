fadfb41899f94b0bd0fcf84d6e14598b4dce8e24
Yao Cui <Aprilcui11@gmail.com>
2016-09-25 13:05:11 -0500

    Add files via upload

--- leetcode.sql
+++ leetcode.sql
@@ -42,3 +42,44 @@ FROM Logs l1, Logs l2, Logs l3
 WHERE l3.Id-l2.Id=1 AND l2.Id-l1.Id=1 AND l3.Num=l2.Num AND l2.Num=l1.Num
 
 /* 178. Rank Scores */
+
+/* Nth Highest Salary */
+/* Answer 1 */
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  set N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT (
+        SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC
+        LIMIT 1 OFFSET N
+      )
+  );
+END
+/* Answer 2 */
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary 
+      FROM Employee e1
+      WHERE N = 
+      (SELECT COUNT(DISTINCT e2.Salary)
+      FROM Employee e2
+      WHERE e2.Salary > e1.Salary)
+  );
+END
+
+/* 185. Department Top Three Salaries */
+SELECT Department.Name AS Department, Table1.Name AS Employee, Table1.Salary
+FROM
+(SELECT *
+FROM Employee e1
+WHERE 3>
+    (SELECT COUNT(DISTINCT e2.Salary)
+    FROM Employee e2
+    WHERE e2.Salary > e1.Salary AND e1.DepartmentId=e2.DepartmentId)) Table1
+JOIN Department
+ON Department.Id=Table1.DepartmentId
+

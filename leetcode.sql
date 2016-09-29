5a2c86a2ad18cf0314b5d72ff1286d493fcb2768
Yao Cui <Aprilcui11@gmail.com>
2016-09-29 10:18:01 -0500

    Add files via upload

--- leetcode.sql
+++ leetcode.sql
@@ -43,7 +43,7 @@ WHERE l3.Id-l2.Id=1 AND l2.Id-l1.Id=1 AND l3.Num=l2.Num AND l2.Num=l1.Num
 
 /* 178. Rank Scores */
 
-/* Nth Highest Salary */
+/* 177. Nth Highest Salary */
 /* Answer 1 */
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
@@ -82,4 +82,33 @@ WHERE 3>
     WHERE e2.Salary > e1.Salary AND e1.DepartmentId=e2.DepartmentId)) Table1
 JOIN Department
 ON Department.Id=Table1.DepartmentId
+
+/* 262. Trips and Users */
+SELECT t.Request_at AS Day, ROUND(SUM(CASE t.Status WHEN 'completed' THEN 0 ELSE 1 END)/COUNT(*),2) AS 'Cancellation Rate'
+FROM Trips t 
+INNER JOIN Users u
+ON t.Client_Id=u.Users_Id
+WHERE u.Banned='No' AND u.Role='client'
+ AND t.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.Request_at
+ORDER BY t.Request_at
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
